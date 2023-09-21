380c49ed8ecc544dde8058d9b74f3dad606e8037
VinhTDo <vinhdo.personal@gmail.com>
2023-09-21 22:31:05 +0800

    Implement Block abstract class

--- Tetris/Block.cs
+++ Tetris/Block.cs
@@ -1,12 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Tetris
 {
-    internal class Block
+    public abstract class Block
     {
+        protected abstract Transform[][] Tiles { get; }
+        protected abstract Transform StartOffset { get; }
+
+        public abstract int Id { get; }
+
+        private int _rotationState = 0;
+        private Transform offset; 
+
+        public Block()
+        {
+            offset = new(StartOffset.Row, StartOffset.Column);
+        }
+
+        public IEnumerable<Transform> GetTileTransforms()
+        {
+            foreach (Transform transform in Tiles[_rotationState]) 
+            {
+                yield return new Transform(transform.Row + offset.Row, transform.Column + offset.Column);
+            }
+        }
+
+        public void RotateClockwise()
+        {
+            _rotationState = (_rotationState + 1) % Tiles.Length;
+        }
+
+        public void RotateCounterClockwise()
+        {
+            if (_rotationState < 0)
+            {
+                _rotationState = Tiles.Length - 1;
+            }
+            else
+            {
+                _rotationState--;
+            }
+        }
+
+        public void Move(int rows, int columns)
+        {
+            offset.Row += rows;
+            offset.Column += columns;
+        }
+
+        public void Reset()
+        {
+            _rotationState = 0;
+            offset.Row = StartOffset.Row;
+            offset.Column = StartOffset.Column;
+        }
     }
 }
